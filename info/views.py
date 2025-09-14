2d344b3fabd27934a5f81dbaa443a783acd94c2f
santtos0x1 <ruanrodrigues393@gmail.com>
2025-09-14 09:38:37 -0300

    CSS implementations

--- info/views.py
+++ info/views.py
@@ -3,35 +3,32 @@ from django.http import JsonResponse
 import locale
 import requests
 
+
 locale.setlocale(locale.LC_ALL, 'en_US.UTF-8')
+
 
 def get_info_api():
     ISS_URL = 'https://api.wheretheiss.at/v1/satellites/25544'
     req = requests.get(ISS_URL)
-    data = req.json()
-    
+    data = req.json() 
     iss_lat = f"{data['latitude']}"
     iss_lon = f"{data['longitude']}"
     iss_velocity = f"{data['velocity']:.2f}"
     iss_altitude = f"{data['altitude']:.2f}"
     iss_visibility = f"{data['visibility']}"
-    
     info = [iss_velocity, iss_altitude, iss_visibility, iss_lat, iss_lon]
     
     return info
 
+
 def info(request):
     data = get_info_api()
-    
     velo = float(data[0])
     velo_formatted = locale.format_string("%d", velo, grouping=True)
-    
     alti = float(data[1])
     alti_formatted = locale.format_string("%d", alti, grouping=True)
-    
     lat = data[3]
     lon = data[4]
-    
     visi = data[2]
     
     return render(request, 'info/pages/info-home.html', context={
@@ -41,6 +38,7 @@ def info(request):
         "lat": lat,
         "lon": lon
     })
+
 
 def iss_info(request):
     return JsonResponse({"inf": get_info_api()})
